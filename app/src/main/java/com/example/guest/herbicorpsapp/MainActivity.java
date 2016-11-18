55def3e061a736a7a516e243cd1738ba598ed5a7
Blake Scala <blakescala@gmail.com>
2016-11-18 08:03:13 -0800

    add custom font to main title

--- app/src/main/java/com/example/guest/herbicorpsapp/MainActivity.java
+++ app/src/main/java/com/example/guest/herbicorpsapp/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.guest.herbicorpsapp;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.mainTitle) TextView mMainTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Typeface pacificoFont = Typeface.createFromAsset(getAssets(), "fonts/Pacifico.ttf");
+        mMainTitle.setTypeface(pacificoFont);
+
     }
 }
