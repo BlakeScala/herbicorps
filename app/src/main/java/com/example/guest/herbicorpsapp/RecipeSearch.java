829dee0d8d7bf2cccd42817aaf05e9330df0ea85
Blake Scala <blakescala@gmail.com>
2016-11-18 10:34:24 -0800

    add custom recipe objects to list of recipes

--- app/src/main/java/com/example/guest/herbicorpsapp/RecipeSearch.java
+++ app/src/main/java/com/example/guest/herbicorpsapp/RecipeSearch.java
@@ -6,27 +6,35 @@ import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class RecipeSearch extends AppCompatActivity {
     @Bind(R.id.searchText) TextView mSearchText;
-    public Recipe[] recipes = {
-            Recipe recipe = new Recipe("Mac and Cashew Cheese", "Sample Instructions", "http://veganyumminess.com/wp-content/uploads/2014/04/Vegan-Mac-and-Cheese-2.jpg"),
-            Recipe recipeTwo = new Recipe("Macaroni and Cashew Cheese", "Here are instructions", "http://veganyumminess.com/wp-content/uploads/2014/04/Vegan-Mac-and-Cheese-2.jpg")
-    }
-};
 
+    public ArrayList<Recipe> recipes = new ArrayList<Recipe>();
+    public ArrayList<Recipe> savedRecipes = new ArrayList<Recipe>();
+
+    Recipe recipe = new Recipe("Mac and Cashew Cheese", "Sample Instructions", "http://veganyumminess.com/wp-content/uploads/2014/04/Vegan-Mac-and-Cheese-2.jpg");
+    Recipe recipeTwo = new Recipe("Macaroni and Cashew Cheese", "Here are instructions", "http://veganyumminess.com/wp-content/uploads/2014/04/Vegan-Mac-and-Cheese-2.jpg");
+    Recipe recipeThree = new Recipe("Cashew Mac & Cheese", "How to make it", "http://veganyumminess.com/wp-content/uploads/2014/04/Vegan-Mac-and-Cheese-2.jpg");
+}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_search);
         ButterKnife.bind(this);
+        recipes.add(recipe);
+        recipes.add(recipeTwo);
+        recipes.add(recipeThree);
 
         Intent intent = getIntent();
         String ingredientsInput = intent.getStringExtra("ingredients").toString();
         mSearchText.setText("Vegan meals including " + ingredientsInput);
         Toast.makeText(RecipeSearch.this, "API Call", Toast.LENGTH_LONG).show();
+
     }
 }
