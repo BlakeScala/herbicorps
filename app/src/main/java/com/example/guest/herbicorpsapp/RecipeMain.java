a8d5cb74d106b79d1b466c0bdfb8766bddae2195
Blake Scala <blakescala@gmail.com>
2016-11-18 11:44:14 -0800

    add recipe main page that goes to specific item of lists recipe page

--- app/src/main/java/com/example/guest/herbicorpsapp/RecipeMain.java
+++ app/src/main/java/com/example/guest/herbicorpsapp/RecipeMain.java
@@ -1,13 +1,25 @@
 package com.example.guest.herbicorpsapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class RecipeMain extends AppCompatActivity {
+    @Bind(R.id.recipeTitle) TextView mRecipeTitle;
+    @Bind(R.id.recipeInstructions) TextView mRecipeInstructions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_main);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String recipe = intent.getStringExtra("selectedRecipe");
+        mRecipeTitle.setText(recipe);
     }
 }
