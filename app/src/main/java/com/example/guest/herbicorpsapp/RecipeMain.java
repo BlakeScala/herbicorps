825cd832ae84ce590249e432af5c38d361ee004b
Blake Scala <blakescala@gmail.com>
2016-11-18 12:27:36 -0800

    remove attribute on xButton so alignment is correct

--- app/src/main/java/com/example/guest/herbicorpsapp/RecipeMain.java
+++ app/src/main/java/com/example/guest/herbicorpsapp/RecipeMain.java
@@ -3,15 +3,24 @@ package com.example.guest.herbicorpsapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class RecipeMain extends AppCompatActivity {
+public class RecipeMain extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.recipeTitle) TextView mRecipeTitle;
     @Bind(R.id.recipeInstructions) TextView mRecipeInstructions;
-    @Bind(R.id.)
+    @Bind(R.id.xButton) Button mXButton;
+    @Bind(R.id.checkButton) Button mCheckButton;
+
+    public ArrayList<String> checkedRecipes = new ArrayList<String>();
+    public String recipeToAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,17 @@ public class RecipeMain extends AppCompatActivity {
 
         Intent intent = getIntent();
         String recipe = intent.getStringExtra("selectedRecipe");
+        recipeToAdd = recipe;
         mRecipeTitle.setText(recipe);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mXButton) {
+            Intent intent = new Intent(RecipeMain.this, RecipeSearch.class);
+        } else if(v == mCheckButton) {
+            checkedRecipes.add(recipeToAdd);
+            Toast.makeText(RecipeMain.this, "Added to Favorites", Toast.LENGTH_LONG).show();
+        }
     }
 }
