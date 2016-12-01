b291dcf1e8f7ec41a8c1b99ed69236449983b97b
Blake Scala <blakescala@gmail.com>
2016-12-01 14:29:46 -0800

    edit recipe model with new attributes from json response

--- app/src/main/java/com/example/guest/herbicorpsapp/Recipe.java
+++ app/src/main/java/com/example/guest/herbicorpsapp/Recipe.java
@@ -1,28 +1,59 @@
 package com.example.guest.herbicorpsapp;
 
-/**
- * Created by Guest on 11/18/16.
- */
-public class Recipe {
-    private String recipeTitle;
-    private String imageURL;
-    private String instructions;
+import java.util.ArrayList;
 
-    public Recipe(String recipeTitle, String instructions, String imageURL) {
-        this.recipeTitle = recipeTitle;
-        this.instructions = instructions;
+public class Recipe {
+    private String recipeName;
+    private String sourceName;
+    private String imageURL;
+    private ArrayList<String> ingredients;
+    private String instructions;
+    private int estimatedTime;
+    private int rating;
+    private String id;
+
+    public Recipe(String recipeName, String sourceName, String imageURL, ArrayList<String> ingredients, String instructions, int estimatedTime, int rating, String id) {
+        this.recipeName = recipeName;
+        this.sourceName = sourceName;
         this.imageURL = imageURL;
+        this.ingredients = ingredients;
+        this.instructions = instructions;
+        this.estimatedTime = estimatedTime;
+        this.rating = rating;
+        this.id = id;
     }
 
-    public String getRecipeTitle() {
-        return recipeTitle;
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    public String getSourceName() {
+        return sourceName;
     }
 
     public String getImageURL() {
         return imageURL;
     }
 
+    public ArrayList<String> getIngredients() {
+        return ingredients;
+    }
+
     public String getInstructions() {
         return instructions;
     }
+
+    public int getEstimatedTime() {
+        return estimatedTime;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+
 }
