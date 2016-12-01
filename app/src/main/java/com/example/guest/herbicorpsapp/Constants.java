268169aa884b24ca1e426d78f10b993571b8f0bf
Blake Scala <blakescala@gmail.com>
2016-12-01 10:34:46 -0800

    add app id and key to gradle.properties, add and connect constants for api call

---
+++ app/src/main/java/com/example/guest/herbicorpsapp/Constants.java
@@ -0,0 +1,9 @@
+package com.example.guest.herbicorpsapp;
+
+/**
+ * Created by Guest on 12/1/16.
+ */
+public class Constants {
+    public static final String APP_KEY = BuildConfig.APP_KEY;
+    public static final String APP_ID = BuildConfig.APP_ID;
+}
