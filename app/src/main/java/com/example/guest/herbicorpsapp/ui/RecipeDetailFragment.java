19985d8f9222350854494862ee05440a2f0ed14d
Blake Scala <blakescala@gmail.com>
2016-12-02 10:26:38 -0800

    bundle all recipe information using parceler in RecipeDetailFragment

--- app/src/main/java/com/example/guest/herbicorpsapp/ui/RecipeDetailFragment.java
+++ app/src/main/java/com/example/guest/herbicorpsapp/ui/RecipeDetailFragment.java
@@ -6,25 +6,62 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.guest.herbicorpsapp.R;
+import com.example.guest.herbicorpsapp.models.Recipe;
+import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RecipeDetailFragment extends Fragment {
+    @Bind(R.id.recipeImageView) ImageView mImageLabel;
+    @Bind(R.id.recipeNameTextView) TextView mNameLabel;
+    @Bind(R.id.ratingTextView) TextView mRatingLabel;
+    @Bind(R.id.estimatedTimeTextView) TextView mTimeLabel;
+    @Bind(R.id.ingredientTextView) TextView mIngredientLabel;
+    @Bind(R.id.checkButton) Button mCheckButton;
+    @Bind(R.id.xButton) Button mXButton;
 
+    private Recipe mRecipe;
 
-    public RecipeDetailFragment() {
-        // Required empty public constructor
+    public static RecipeDetailFragment newInstance(Recipe recipe) {
+        RecipeDetailFragment recipeDetailFragment = new RecipeDetailFragment();
+        Bundle args = new Bundle();
+        args.putParcelable("recipe", Parcels.wrap(recipe));
+        recipeDetailFragment.setArguments(args);
+        return recipeDetailFragment;
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mRecipe = Parcels.unwrap(getArguments().getParcelable("recipe"));
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipe_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_recipe_detail, container, false);
+        ButterKnife.bind(this, view);
+
+        Picasso.with(view.getContext()).load(mRecipe.getImageURL()).into(mImageLabel);
+
+        mNameLabel.setText(mRecipe.getRecipeName());
+        mRatingLabel.setText(String.valueOf(mRecipe.getRating()));
+        mTimeLabel.setText(String.valueOf(mRecipe.getEstimatedTime()));
+        mIngredientLabel.setText(android.text.TextUtils.join(", ", mRecipe.getIngredients()));
+
+        return view;
+
     }
 
 }
