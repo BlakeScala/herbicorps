18f7186559e89c3ace8c09f29c5d73337b50e633
Blake Scala <blakescala@gmail.com>
2016-12-09 10:40:14 -0800

    add viewHolder for saved recipes

---
+++ app/src/main/java/com/example/guest/herbicorpsapp/adapters/FirebaseRecipeViewHolder.java
@@ -0,0 +1,85 @@
+package com.example.guest.herbicorpsapp.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.guest.herbicorpsapp.Constants;
+import com.example.guest.herbicorpsapp.R;
+import com.example.guest.herbicorpsapp.models.Recipe;
+import com.example.guest.herbicorpsapp.ui.RecipeDetailActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Guest on 12/9/16.
+ */
+public class FirebaseRecipeViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    private static final int MAX_WIDTH = 200;
+    private static final int MAX_HEIGHT = 200;
+
+    View mView;
+    Context mContext;
+
+    public FirebaseRecipeViewHolder(View itemView) {
+        super(itemView);
+        mView = itemView;
+        mContext = itemView.getContext();
+        itemView.setOnClickListener(this);
+    }
+
+    public void bindRecipe(Recipe recipe) {
+        ImageView recipeImageView = (ImageView) mView.findViewById(R.id.recipeImageView);
+        TextView nameTextView = (TextView) mView.findViewById(R.id.recipeNameTextView);
+        TextView ratingTextView = (TextView) mView.findViewById(R.id.ratingTextView);
+        TextView timeTextView = (TextView) mView.findViewById(R.id.estimatedTimeTextView);
+
+        Picasso.with(mContext)
+                .load(recipe.getImageURL())
+                .resize(MAX_WIDTH, MAX_HEIGHT)
+                .centerCrop()
+                .into(recipeImageView);
+
+        nameTextView.setText(recipe.getRecipeName());
+        ratingTextView.setText("Rating: " + recipe.getRating() + "/5");
+        timeTextView.setText("Time: " + String.valueOf(recipe.getEstimatedTime()/60) + " minutes");
+    }
+
+    @Override
+    public void onClick(View v) {
+        final ArrayList<Recipe> recipes = new ArrayList<>();
+        DatabaseReference ref =  FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_RECIPES);
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    recipes.add(snapshot.getValue(Recipe.class));
+                }
+
+                int itemPosition = getLayoutPosition();
+
+                Intent intent = new Intent(mContext, RecipeDetailActivity.class);
+                intent.putExtra("position", itemPosition + "");
+                intent.putExtra("recipes", Parcels.wrap(recipes));
+
+                mContext.startActivity(intent);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
