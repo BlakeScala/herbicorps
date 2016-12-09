71abe5f00b1441a980fe89f7457ebf714e426941
Blake Scala <blakescala@gmail.com>
2016-12-09 10:42:50 -0800

    add Favorites activity

---
+++ app/src/main/java/com/example/guest/herbicorpsapp/ui/FavoriteRecipeListActivity.java
@@ -0,0 +1,12 @@
+package com.example.guest.herbicorpsapp.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FavoriteRecipeListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
