a5fe2b4b8fb3b09b964e5fd0a7f7a52519977223
Blake Scala <blakescala@gmail.com>
2016-12-11 16:11:59 -0800

    add activity for account creation

---
+++ app/src/main/java/com/example/guest/herbicorpsapp/ui/CreateAccountActivity.java
@@ -0,0 +1,15 @@
+package com.example.guest.herbicorpsapp.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.guest.herbicorpsapp.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
