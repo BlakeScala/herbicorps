3b380e540b47d2a8e6687f056de6a872e8facd00
Blake Scala <blakescala@gmail.com>
2016-12-16 11:13:43 -0800

    refactor recipeList into a fragment

--- app/src/main/java/com/example/guest/herbicorpsapp/ui/RecipeListActivity.java
+++ app/src/main/java/com/example/guest/herbicorpsapp/ui/RecipeListActivity.java
@@ -29,109 +29,10 @@ import okhttp3.Callback;
 import okhttp3.Response;
 
 public class RecipeListActivity extends AppCompatActivity {
-    @Bind(R.id.recipeRecyclerView) RecyclerView mRecipeRecyclerView;
-    private static final String TAG = RecipeListActivity.class.getSimpleName();
-    public ArrayList<Recipe> mRecipes;
-    private RecipeListAdapter mAdapter;
-
-    private SharedPreferences mSharedPreferences;
-    private SharedPreferences.Editor mEditor;
-    private String mRecentFood;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipes);
-        ButterKnife.bind(this);
-
-        Intent intent = getIntent();
-
-        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        mRecentFood = mSharedPreferences.getString(Constants.PREFERENCES_FOOD_KEY, null);
-//        Log.v("prefs", mRecentFood);
-
-        if (mRecentFood != null) {
-            getRecipes(mRecentFood);
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_search, menu);
-        ButterKnife.bind(this);
-
-        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        mEditor = mSharedPreferences.edit();
-
-        MenuItem menuItem = menu.findItem(R.id.action_search);
-        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                addToSharedPreferences(query);
-                getRecipes(query);
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return false;
-            }
-        });
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void addToSharedPreferences(String foodSearch) {
-        mEditor.putString(Constants.PREFERENCES_FOOD_KEY, foodSearch).apply();
-    }
-
-    private void getRecipes(String foodInput) {
-        final YummlyService yummlyService = new YummlyService();
-        yummlyService.findRecipes(foodInput, new Callback() {
-
-            @Override
-            public void onFailure(Call call, IOException e) {
-                e.printStackTrace();
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) {
-                mRecipes = yummlyService.processResults(response);
-
-                RecipeListActivity.this.runOnUiThread(new Runnable() {
-
-//                    @Override
-//                    public void run() {
-//                        String[] recipeNames = new String[mRecipes.size()];
-//                        for (int i = 0; i < recipeNames.length; i ++) {
-//                            recipeNames[i] = mRecipes.get(i).getRecipeName();
-//                        }
-//
-//                        ArrayAdapter adapter = new ArrayAdapter(RecipeListActivity.this, android.R.layout.simple_list_item_1, recipeNames);
-//                        mRecipeListView.setAdapter(adapter);
-//
-//                        for (String recipeName : recipeNames) {
-//                            Log.d("Attributes", "name: " + recipeName);
-//                        }
-//                    }
-                    @Override
-                    public void run() {
-                        mAdapter = new RecipeListAdapter(getApplicationContext(), mRecipes);
-                        mRecipeRecyclerView.setAdapter(mAdapter);
-                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(RecipeListActivity.this);
-                        mRecipeRecyclerView.setLayoutManager(layoutManager);
-                        mRecipeRecyclerView.setHasFixedSize(true);
-                    }
-                });
-            }
-        });
     }
 }
---
+++ app/src/main/java/com/example/guest/herbicorpsapp/ui/RecipeListFragment.java
@@ -0,0 +1,139 @@
+package com.example.guest.herbicorpsapp.ui;
+
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.guest.herbicorpsapp.Constants;
+import com.example.guest.herbicorpsapp.R;
+import com.example.guest.herbicorpsapp.adapters.RecipeListAdapter;
+import com.example.guest.herbicorpsapp.models.Recipe;
+import com.example.guest.herbicorpsapp.services.YummlyService;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RecipeListFragment extends Fragment {
+    @Bind(R.id.recipeRecyclerView) RecyclerView mRecyclerView;
+
+    public ArrayList<Recipe> mRecipes;
+    private RecipeListAdapter mAdapter;
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+    private String mRecentFood;
+
+
+    public RecipeListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        mEditor = mSharedPreferences.edit();
+
+        setHasOptionsMenu(true);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_recipe_list,container, false);
+        ButterKnife.bind(this, view);
+
+        mRecentFood = mSharedPreferences.getString(Constants.PREFERENCES_FOOD_KEY, null);
+
+        if (mRecentFood != null) {
+            getRecipes(mRecentFood);
+        }
+        return view;
+    }
+
+    private void getRecipes(String foodInput) {
+        final YummlyService yummlyService = new YummlyService();
+        yummlyService.findRecipes(foodInput, new Callback() {
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) {
+                mRecipes = yummlyService.processResults(response);
+
+                getActivity().runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        mAdapter = new RecipeListAdapter(getActivity(), mRecipes);
+                        mRecyclerView.setAdapter(mAdapter);
+                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+                        mRecyclerView.setLayoutManager(layoutManager);
+                        mRecyclerView.setHasFixedSize(true);
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_search, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                addToSharedPreferences(query);
+                getRecipes(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+    }
+
+
+    private void addToSharedPreferences(String foodSearch) {
+        mEditor.putString(Constants.PREFERENCES_FOOD_KEY, foodSearch).apply();
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+}
