95f937147b5efe489b8a665d9ead69e2ed00f351
Blake Scala <blakescala@gmail.com>
2016-12-16 09:45:54 -0800

    update firebaseAdapter setup for recipe favorites

--- app/src/main/java/com/example/guest/herbicorpsapp/ui/FavoriteRecipeListActivity.java
+++ app/src/main/java/com/example/guest/herbicorpsapp/ui/FavoriteRecipeListActivity.java
@@ -4,11 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 
 import com.example.guest.herbicorpsapp.Constants;
 import com.example.guest.herbicorpsapp.R;
+import com.example.guest.herbicorpsapp.adapters.FirebaseRecipeListAdapter;
 import com.example.guest.herbicorpsapp.adapters.FirebaseRecipeViewHolder;
 import com.example.guest.herbicorpsapp.models.Recipe;
+import com.example.guest.herbicorpsapp.util.OnStartDragListener;
+import com.example.guest.herbicorpsapp.util.SimpleItemTouchHelperCallback;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -18,9 +22,10 @@ import com.google.firebase.database.FirebaseDatabase;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class FavoriteRecipeListActivity extends AppCompatActivity {
+public class FavoriteRecipeListActivity extends AppCompatActivity implements OnStartDragListener {
     private DatabaseReference mRecipeReference;
-    private FirebaseRecyclerAdapter mFirebaseAdapter;
+    private FirebaseRecipeListAdapter mFirebaseAdapter;
+    private ItemTouchHelper mItemTouchHelper;
 
     @Bind(R.id.favoritesRecyclerView) RecyclerView mRecyclerView;
 
@@ -40,22 +45,39 @@ public class FavoriteRecipeListActivity extends AppCompatActivity {
     }
 
     private void setUpFirebaseAdapter() {
-        mFirebaseAdapter = new FirebaseRecyclerAdapter<Recipe, FirebaseRecipeViewHolder>
-                (Recipe.class, R.layout.recipe_list_item_drag, FirebaseRecipeViewHolder.class, mRecipeReference) {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = user.getUid();
 
-            @Override
-            protected void populateViewHolder(FirebaseRecipeViewHolder viewHolder, Recipe model, int position) {
-                viewHolder.bindRecipe(model);
-            }
-        };
+        mRecipeReference = FirebaseDatabase
+                .getInstance()
+                .getReference(Constants.FIREBASE_CHILD_RECIPES)
+                .child(uid);
+
+        mFirebaseAdapter = new FirebaseRecipeListAdapter(Recipe.class, R.layout.recipe_list_item_drag, FirebaseRecipeViewHolder.class, mRecipeReference, this, this);
+
+
+//            @Override
+//            protected void populateViewHolder(FirebaseRecipeViewHolder viewHolder, Recipe model, int position) {
+//                viewHolder.bindRecipe(model);
+//            }
+
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mRecyclerView.setAdapter(mFirebaseAdapter);
+
+        ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(mFirebaseAdapter);
+        mItemTouchHelper = new ItemTouchHelper(callback);
+        mItemTouchHelper.attachToRecyclerView(mRecyclerView);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         mFirebaseAdapter.cleanup();
+    }
+
+    @Override
+    public void onStartDrag(RecyclerView.ViewHolder viewHolder) {
+        mItemTouchHelper.startDrag(viewHolder);
     }
 }
